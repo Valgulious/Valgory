30859f33f90e8077929d2bece157ddc421d64be5
Valgulious <dmitryfedorenkof@gmail.com>
2018-05-04 21:12:53 +1000

    Create Abstract Factory

--- Patterns/main.cpp
+++ Patterns/main.cpp
@@ -2,6 +2,52 @@
 
 using namespace std;
 
+class Male
+{
+public:
+    virtual void info() = 0;
+    virtual ~Male() {}
+};
+
+class Female
+{
+public:
+    virtual void info() = 0;
+    virtual ~Female() {}
+};
+
+class MalePerson: public Male
+{
+public:
+    void info()
+    {
+        cout << "Gender - Male" << endl;
+    }
+};
+
+class FemalePerson: public Female
+{
+public:
+    void info()
+    {
+        cout << "Gender - Female" << endl;
+    }
+};
+
+class PersonFactory {
+public:
+   Male* createMale()
+   {
+       return new MalePerson;
+   }
+    virtual Female* createFemale()
+    {
+        return new FemalePerson;
+    }
+};
+
+
 int main() {
+
     return 0;
 }
